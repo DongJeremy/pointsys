7963db4b74f5a096d62e137df4a6e66575d38a8e
d05660 <d05660@163.com>
2020-06-19 22:56:06 +0800

    fix start fail.
    Signed-off-by: d05660 <d05660@163.com>

---
+++ pointsys-api/src/main/java/jp/co/nri/point/api/service/impl/JobServiceImpl.java
@@ -0,0 +1,10 @@
+package jp.co.nri.point.api.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import jp.co.nri.point.api.service.JobService;
+
+@Service
+public class JobServiceImpl implements JobService {
+
+}
